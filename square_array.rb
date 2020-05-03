9bc31b91198d590d61aece364f11645311bb2099
Richard Hanolt <rhanolt@gmail.com>
2020-05-03 05:16:03 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,13 +1,5 @@
-=begin
 def square_array(numbers)
   new_numbers = Array.new
   numbers.each{|items| new_numbers << items**2}
-  return new_numbers
-end
-=end
-
-def square_array(numbers)
-  new_numbers = Array.new
-  new_numbers.collect{|items| items**2}
   return new_numbers
 end
